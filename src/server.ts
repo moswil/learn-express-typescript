73ff15194c741a0cf10936c4e0604f3f2369993b
moswil <moseswillfred1@gmail.com>
2020-07-29 07:58:40 +0300

    ch(set-up-project): setup the project
    - Ensure logging is working fine for all environments
    - Sets up use of environment variables and validates them
    
    [ Finishes #1 ]

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import 'dotenv/config';
 import App from './app';
 import UserController from './user/user.controller';
+import validateEnv from './utils/validateEnv';
+
+validateEnv();
 
 const app = new App([new UserController()]);
 
